9c84f7be2d563dc97a6fde9ee9af2c8edb219fb1
pavithra prabodha <pavithrarox1@gmail.com>
2018-01-07 19:45:25 +0530

    hello world, single component

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,23 @@
+import React from "react";
+import { render } from "react-dom";
 
-//to test the application works
-console.log("it works")
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12"> <h1>Hello!</h1></div>
+                </div>
+               
+            </div>
+            
+        );
+    }
+
+}
+
+
+//should be the name of the class
+render(<App/>, window.document.getElementById("app"));
