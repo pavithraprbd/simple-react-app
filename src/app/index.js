872ea6a0bb02c98aecc613add51acb4962fbb860
Prabodha Pathirathna <prabodha@affno.lk>
2018-02-26 17:38:32 +0530

    test

--- src/app/index.js
+++ src/app/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import { render } from "react-dom";
 
 
-class App extends React.Component{
+class App extends React.Component {
 
-    render(){
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12"> <h1>Hello!</h1></div>
-                </div>
-               
-            </div>
-            
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            div className = "row" >
+            <
+            div className = "col-md-12" > < h1 > Hello! < /h1></div >
+            <
+            /div>
+
+            <
+            /div>
+
         );
     }
 
@@ -20,4 +24,4 @@ class App extends React.Component{
 
 
 //should be the name of the class
-render(<App/>, window.document.getElementById("app"));
+render( < App / > , window.document.getElementById("app"));
